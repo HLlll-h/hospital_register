ed0ef8df8a84711f11d5da5db2a554529e2577ff
HLlll <61738332+HLlll-h@users.noreply.github.com>
2022-03-04 17:07:44 +0800

    Update Test.java

--- service/service_oss/src/test/java/Test.java
+++ service/service_oss/src/test/java/Test.java
@@ -6,7 +6,7 @@ public class Test {
     public static void main(String[] args) {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
-        String accessKeyId = "LTAI5tSDaQNefdLcYGL5c3hB";
+        String accessKeyId = ""; //accessKeyId值是LTAI5tSDaQNefdLcYGL5c3hB
         String accessKeySecret = "eB2sSBGjZuna6BG2l1gkMi1kiUXJ5z";
         String bucketName = "hospital-test";
 
